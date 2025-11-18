720a9fb97ca069f55768aecff584a81cee58cbfb
Michael_DWL <michaeldouglas.devs@gmail.com>
2025-11-17 21:47:49 -0300

    feat : home page

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,7 +1,17 @@
 const btnClose = document.querySelector('.modal-task');
 const task = document.querySelector('.task');
+const menu = document.querySelector('.menu')
+const menuCollapsed = document.querySelector('.menu-collapsed');
 
+function closeMenu() { 
+    menu.style.display = 'none';
+    menuCollapsed.style.display = 'flex';
+}
 
+function openMenu() {
+    menu.style.display = 'flex';
+    menuCollapsed.style.display = 'none';
+}
 
 function closeModal() {
     btnClose.style.display = 'none';
